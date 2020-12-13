dff8c757cb1d8001518892eea9a3636f4e607785
BuildTools <unconfigured@null.spigotmc.org>
2020-12-13 19:34:52 +0100

    set up ChatListener

--- Chatcolors/src/de/kyushi/chatcolors/chatListener/ChatListener.java
+++ Chatcolors/src/de/kyushi/chatcolors/chatListener/ChatListener.java
@@ -1,4 +1,30 @@
 package de.kyushi.chatcolors.chatListener;
 
-public class ChatListener {
-}
+import de.kyushi.chatcolors.arrayLists.ArrayLists;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.ArrayList;
+
+    public class ChatListener implements Listener {
+
+        public static ArrayList<String> activePlayers = ArrayLists.getActivePlayers();
+        @EventHandler
+
+        public void handleMutedChat(AsyncPlayerChatEvent event) {
+            Player player = event.getPlayer();
+            String message = event.getMessage().replace("%", "%%");
+            message = ChatColor.translateAlternateColorCodes('&', message);
+
+            if (activePlayers.contains(player.getName())) {
+                event.setFormat("§6[§f" + player.getName() + "§6] » §a" + message);
+            } else {
+                event.setFormat("§6[§f" + player.getName() + "§6] » §f" + message);
+            }
+
+        }
+    }
+
