fb867fe2b44fa1f0086381c471aafb1ad3e8c5b0
BuildTools <unconfigured@null.spigotmc.org>
2020-12-13 03:35:21 +0100

    Initial commit

---
+++ Chatcolors/src/de/kyushi/chatcolors/main/Main.java
@@ -0,0 +1,21 @@
+package de.kyushi.chatcolors.main;
+
+import de.kyushi.chatcolors.commands.Command;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public class Main extends JavaPlugin {
+
+
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("§6[§aTest Plugin§6] §bPlugin wurde erfolgreich gestartet!");
+        Objects.requireNonNull(this.getCommand("color")).setExecutor(new Command());
+
+    }
+
+    public void onDisable() {
+        System.out.println("§6[§aChat Colors§6] §4Plugin wird deaktiviert!");
+    }
+}
